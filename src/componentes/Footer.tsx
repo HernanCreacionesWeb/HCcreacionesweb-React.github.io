f183e4391e03e97ef529c0dd68af7b17a8d00b48
HernanCreacionesWeb <hccreacionesweb@gmail.com>
2024-12-23 16:19:35 -0300

    Linkea el logo en el footer a la misma ruta que el header

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <a href="https://wa.me/2616655144" target="_blank" rel="noopener noreferrer" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <a href="#inicio" target="_blank" rel="noopener noreferrer" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             <img src={logo} height="100" alt="logo" />
           </a>
         </div>
